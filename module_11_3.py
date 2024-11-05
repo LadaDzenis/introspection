ed2e39703513510964b81db34466b31fae5f45fc
LadaDzenis <lada.tranevskaya@mail.ru>
2024-11-05 17:45:22 +0300

    Add files via upload

--- module_11_3.py
+++ module_11_3.py
@@ -1,12 +1,13 @@
+import inspect
 from pprint import pprint
 
 
 def introspection_info(obj):
     dict_ = {}
-    attributes_obj = dir(obj)
-    methods_obj = [method for method in attributes_obj if callable(getattr(obj, method))]
-    module_obj = obj.__class__.__module__
-    dict_['type'] = type(obj)
+    methods_obj = [method for method in dir(obj) if callable(getattr(obj, method))]
+    attributes_obj = [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
+    module_obj = inspect.getmodule(obj)
+    dict_['type'] = type(obj).__name__
     dict_['attributes'] = attributes_obj
     dict_['methods'] = methods_obj
     dict_['module'] = module_obj
